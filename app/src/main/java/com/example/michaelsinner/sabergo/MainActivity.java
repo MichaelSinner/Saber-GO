f8786b7ce1bc40f17625867fab1b2017e7799c09
Michael Andres Peñaloza <pachoim@hotmail.com>
2016-12-11 02:47:49 -0500

    AP01 - Crear Repositorio en Github
    Se realiza la creación del repositorio donde esta almacenado el proyecto
    Saber Go.
    
    Michael Andres Peñaloza

---
+++ app/src/main/java/com/example/michaelsinner/sabergo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.michaelsinner.sabergo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    Button btnStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = 
+    }
+}
