81adc193abbf39a04e553107a9e674c157ad3826
Michael Andres Peñaloza <pachoim@hotmail.com>
2017-02-19 00:06:23 -0500

    AP02 - Actualización de proyecto
    Actualizacion de proyecto para versión 2.2.3 de Android Studio

--- app/src/main/java/com/example/michaelsinner/sabergo/Index.java
+++ app/src/main/java/com/example/michaelsinner/sabergo/Index.java
@@ -1,13 +1,55 @@
 package com.example.michaelsinner.sabergo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Index extends AppCompatActivity {
+public class Index extends Activity
+{
+    Button btnprueba;
+    TextView tvTitulo;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+
+
+
+        ActionBar actionBar = getActionBar();
+
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View customView = inflater.inflate(R.layout.actionbar_home, null);
+
+
+
+        ImageView ivExit = (ImageView) customView.findViewById(R.id.ivExit);
+        ivExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+        btnprueba = (Button) findViewById(R.id.btnLogin);
+        btnprueba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intetn = new Intent(Index.this, Login.class);
+                startActivity(intetn);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/michaelsinner/sabergo/MainActivity.java
+++ app/src/main/java/com/example/michaelsinner/sabergo/MainActivity.java
@@ -1,20 +1,49 @@
 package com.example.michaelsinner.sabergo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends Activity
 {
 
     Button btnStart;
+    TextView tvInicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        btnStart = 
+        /*
+        * se referencia la parte logica y visual del Button btnStart y se implenta su evento del click
+        * */
+        //Typeface font = Typeface.createFromAsset(getAssets(),"fonts/Sanlabello.ttf");
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(toIndex());
+            }
+        });
+        //btnStart.setTypeface(font);
+
+
+
+    }
+
+    public Intent toIndex()
+    {
+        Intent toIndex = new Intent(MainActivity.this, Index.class);
+        return toIndex;
     }
 }
